56960b6b8923aab0dbd13c59bd4a4f238cb51442
liufeng_knight@163.com <liufeng_knight@163.com>
2026-02-14 11:14:11 +0800

    add HXPICKER_ENABLE_CORE_IMAGEVIEW_KF

--- Package.swift
+++ Package.swift
@@ -22,7 +22,8 @@ let package = Package(
                 .define("HXPICKER_ENABLE_SPM"),
                 .define("HXPICKER_ENABLE_PICKER"),
                 .define("HXPICKER_ENABLE_EDITOR"),
-                .define("HXPICKER_ENABLE_CAMERA")
+                .define("HXPICKER_ENABLE_CAMERA"),
+                .define("HXPICKER_ENABLE_CORE_IMAGEVIEW_KF")
             ]),
     ]
 )
